fe86fa7dd33f4977a3550753dc93d224bee6d702
Delevone <grumpysucks@gmail.com>
2026-09-04 14:03:33 -0500

    Updated cal.py get_month_info function to be more fluid to month info rather than hard coding weeks in advance

--- app/cal.py
+++ app/cal.py
@@ -6,12 +6,13 @@ def get_month_info():
     current_year = today.strftime("%Y")
     current_month = today.strftime("%m") if today.strftime("%m")[0] != "0" else today.strftime("%m")[1:]
     cal = calendar.Calendar(6)
-    info = [[],[],[],[],[],[]]
-    week = 0
-    for i in cal.itermonthdays(int(current_year), int(current_month)):
-        if len(info[week]) < 7:
-            info[week].append(i)
-        else:
-            week += 1
-            info[week].append(i)
+    info = []
+    month_iter = list(cal.itermonthdays(int(current_year), int(current_month)))
+    i = 0
+    while i < len(month_iter):
+        week = []
+        while len(week) < 7 and i < len(month_iter):
+            week.append(month_iter[i])
+            i += 1
+        info.append(week)
     return info, today.strftime("%B") + " " + current_year
